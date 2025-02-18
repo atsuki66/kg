d4a81707d0ed4bc9fc3b3e026db7a5b81ca2495c
unknown <bistrov.seryozha@yandex.ru>
2025-02-18 12:55:01 +0300

    ура... победа
    # Conflicts:
    #	src~HEAD
    #	target~HEAD
    #	tests~HEAD

---
+++ src/main/java/com/cgvsu/math/Vector2f.java
@@ -0,0 +1,17 @@
+package com.cgvsu.math;
+
+// Это заготовка для собственной библиотеки для работы с линейной алгеброй
+public class Vector2f {
+    public Vector2f(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int ssdf (){return 213123;}
+
+    public int asdasdad(){return 123;}
+    public float x, y;
+    public int hi(){
+        return 4;
+    }
+}
